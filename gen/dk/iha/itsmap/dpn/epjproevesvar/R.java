27f2a9609b03201302f2ed95e9ab6ba1a420aa2d
Dennis Thomasen <dennisthomasen@gmail.com>
2012-12-03 14:29:54 +0100

    Login screen added, patientoverview added

--- gen/dk/iha/itsmap/dpn/epjproevesvar/R.java
+++ gen/dk/iha/itsmap/dpn/epjproevesvar/R.java
@@ -14,41 +14,63 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int CPRLabel=0x7f070006;
-        public static final int CPROutput=0x7f070007;
-        public static final int ColorLabel=0x7f070008;
-        public static final int ColorOutput=0x7f070009;
-        public static final int FilterText=0x7f070003;
-        public static final int NameLabel=0x7f070004;
-        public static final int NameOutput=0x7f070005;
-        public static final int NotificationModeLabel=0x7f07000a;
-        public static final int NotificationOutput=0x7f07000b;
-        public static final int logout=0x7f070002;
-        public static final int menu_settings=0x7f07000c;
-        public static final int mypatients=0x7f070000;
-        public static final int patientsinformation=0x7f070001;
+        public static final int CPRLabel=0x7f07000d;
+        public static final int CPROutput=0x7f07000e;
+        public static final int ColorLabel=0x7f07000f;
+        public static final int ColorOutput=0x7f070010;
+        public static final int FilterText=0x7f07000a;
+        public static final int NameLabel=0x7f07000b;
+        public static final int NameOutput=0x7f07000c;
+        public static final int NotificationModeLabel=0x7f070011;
+        public static final int NotificationOutput=0x7f070012;
+        public static final int email=0x7f070003;
+        public static final int login=0x7f070005;
+        public static final int login_form=0x7f070002;
+        public static final int login_status=0x7f070000;
+        public static final int login_status_message=0x7f070001;
+        public static final int logout=0x7f070009;
+        public static final int menu_forgot_password=0x7f070014;
+        public static final int menu_settings=0x7f070013;
+        public static final int mypatients=0x7f070007;
+        public static final int password=0x7f070004;
+        public static final int patientsinformation=0x7f070008;
+        public static final int sign_in_button=0x7f070006;
     }
     public static final class layout {
-        public static final int activity_main_menu=0x7f030000;
-        public static final int activity_my_patients=0x7f030001;
-        public static final int activity_patient_information=0x7f030002;
-        public static final int activity_patient_overview=0x7f030003;
-        public static final int activity_test=0x7f030004;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main_menu=0x7f030001;
+        public static final int activity_my_patients=0x7f030002;
+        public static final int activity_patient_information=0x7f030003;
+        public static final int activity_patient_overview=0x7f030004;
         public static final int customlistview=0x7f030005;
     }
     public static final class menu {
         public static final int activity_favorite_patients=0x7f060000;
-        public static final int activity_main_menu=0x7f060001;
-        public static final int activity_my_patients=0x7f060002;
-        public static final int activity_patient_information=0x7f060003;
-        public static final int activity_patient_overview=0x7f060004;
-        public static final int activity_test=0x7f060005;
+        public static final int activity_login=0x7f060001;
+        public static final int activity_main_menu=0x7f060002;
+        public static final int activity_my_patients=0x7f060003;
+        public static final int activity_patient_information=0x7f060004;
+        public static final int activity_patient_overview=0x7f060005;
+        public static final int activity_test=0x7f060006;
     }
     public static final class string {
+        public static final int action_sign_in_register=0x7f04000b;
+        public static final int action_sign_in_short=0x7f04000c;
         public static final int app_name=0x7f040000;
+        public static final int error_field_required=0x7f040012;
+        public static final int error_incorrect_password=0x7f040011;
+        public static final int error_invalid_email=0x7f04000f;
+        public static final int error_invalid_password=0x7f040010;
         public static final int hello_world=0x7f040001;
+        public static final int login_progress_signing_in=0x7f04000e;
+        public static final int menu_forgot_password=0x7f04000d;
         public static final int menu_settings=0x7f040002;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f040009;
+        public static final int prompt_password=0x7f04000a;
         public static final int title_activity_favorite_patients=0x7f040004;
+        public static final int title_activity_login=0x7f040008;
         public static final int title_activity_my_patients=0x7f040003;
         public static final int title_activity_patient_information=0x7f040005;
         public static final int title_activity_patient_overview=0x7f040007;
@@ -58,11 +80,13 @@ public final class R {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -81,5 +105,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
+        public static final int LoginFormContainer=0x7f050002;
     }
 }
